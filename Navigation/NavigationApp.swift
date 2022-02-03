98a8c581a7d9141309bd3622733685ac19af69c2
polevdhikova <julia8978@gmail.com>
2022-02-03 16:30:20 +0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Юлия Полевщикова on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
