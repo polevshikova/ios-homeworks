3c8c1ff898c2e2ab6a6c76a8e0552383fb19c5f6
polevdhikova <julia8978@gmail.com>
2022-04-20 20:22:12 +0300

    "edit progile header"

--- App3/App3/Navigation/Profile/ProfileHeaderView.swift
+++ App3/App3/Navigation/Profile/ProfileHeaderView.swift
@@ -7,153 +7,120 @@
 
 import UIKit
 
-class ProfileHeaderView: UIView, UITextFieldDelegate {
+final class ProfileHeaderView: UIView {
     
-    weak var profileViewController: ProfileViewController?
-        
-    private lazy var avatarImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.toAutoLayout()
-        imageView.image = UIImage(named: "photo.jpeg")
-        imageView.layer.cornerRadius = 40
-        imageView.layer.borderWidth = 3
+    lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "photo.jpeg"))
+        imageView.layer.cornerRadius = 75
         imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 3
         imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     private lazy var fullNameLabel: UILabel = {
         let label = UILabel()
-        label.toAutoLayout()
-        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        label.text = titleText
-        label.textAlignment = .left
+        label.text = "Юля"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var statusLabel: UILabel = {
         let label = UILabel()
-        label.toAutoLayout()
-        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.text = "show status"
+        label.font = .systemFont(ofSize: 14, weight: .regular)
         label.textColor = .gray
-        label.text = statusText
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var setStatusButton: UIButton = {
         let button = UIButton()
-        button.toAutoLayout()
-        button.setTitle("show status", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .blue
-        button.layer.cornerRadius = 4
-        button.layer.shadowOffset.width = 4
-        button.layer.shadowOffset.height = 4
-        button.layer.shadowRadius = 4
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOpacity = 0.7
-        button.addTarget(self, action: #selector(didTapStatusButton), for: .touchUpInside)
+        button.setTitle("change status", for: .normal)
+        button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)
+        button.layer.cornerRadius = 10
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(self.didTapSetStatusButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     private lazy var statusTextField: UITextField = {
         let textField = UITextField()
-        textField.toAutoLayout()
-        textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        textField.textColor = .black
-        textField.layer.cornerRadius = 12
-        textField.layer.masksToBounds = true
-        textField.layer.borderWidth = 1
-        textField.layer.borderColor = UIColor.black.cgColor
+        textField.placeholder = "writing something..."
+        textField.returnKeyType = .done
+        textField.autocapitalizationType = .words
+        textField.font = .systemFont(ofSize: 15)
+        textField.textColor = .systemGray2
+        textField.backgroundColor = .systemGray4
+        textField.borderStyle = .roundedRect
+        textField.returnKeyType = .next
+        textField.keyboardType = .default
+        textField.clearButtonMode = .always
+        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
-    
-    private lazy var labelsStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.toAutoLayout()
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-        stackView.spacing = 10
-        return stackView
-    }()
-    
-    private lazy var infoStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.toAutoLayout()
-        stackView.axis = .horizontal
-        stackView.spacing = 20
-        return stackView
-    }()
-    
-    private lazy var alphaView: UIView = {
-        let view = UIView()
-        view.toAutoLayout()
-        view.backgroundColor = .white
-        return view
-    }()
-    
-    private var statusText: String = "waiting for something..."
-    private var titleText: String = "Юля"
-    
+        
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.statusTextField.delegate = self
         self.drawSelf()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapKeyboardOff(_:)))
+        self.addGestureRecognizer(tap)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func drawSelf(){
-
-        self.addSubviews(self.infoStackView, self.setStatusButton, self.statusTextField)
-        self.infoStackView.addArrangedSubviews(self.avatarImageView, self.labelsStackView)
-        self.labelsStackView.addArrangedSubviews(self.fullNameLabel, self.statusLabel)
-                
+    private func drawSelf() {
+        addSubview(avatarImageView)
+        addSubview(fullNameLabel)
+        addSubview(statusLabel)
+        addSubview(setStatusButton)
+        addSubview(statusTextField)
+        
         NSLayoutConstraint.activate([
-            self.infoStackView.topAnchor.constraint(equalTo: self.topAnchor),
-            self.infoStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -105),
-            self.infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            self.infoStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            
-            self.avatarImageView.heightAnchor.constraint(equalTo: self.avatarImageView.widthAnchor, multiplier: 1.0),
-            
-            self.setStatusButton.topAnchor.constraint(equalTo: self.statusTextField.bottomAnchor, constant: 5),
-            self.setStatusButton.leadingAnchor.constraint(equalTo: self.infoStackView.leadingAnchor),
-            self.setStatusButton.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor),
-            self.setStatusButton.heightAnchor.constraint(equalToConstant: 50),
-            
-            self.statusTextField.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 5),
-            self.statusTextField.leadingAnchor.constraint(equalTo: self.statusLabel.leadingAnchor),
-            self.statusTextField.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor),
-            self.statusTextField.heightAnchor.constraint(equalToConstant: 35)
-        ].compactMap({$0}))
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 150),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 150),
+            fullNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
+            fullNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 182),
+            fullNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
+            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            setStatusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -16),
+            statusTextField.leadingAnchor.constraint(equalTo: statusLabel.leadingAnchor),
+            statusTextField.trailingAnchor.constraint(equalTo: statusLabel.trailingAnchor),
+            statusTextField.heightAnchor.constraint(equalToConstant: 34),
+            statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -16),
+            statusLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            statusLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor)
+        ])
     }
     
-    func hideKeyboard() {
-        self.statusTextField.resignFirstResponder()
+    @objc func tapKeyboardOff(_ sender: Any) {
+        statusTextField.resignFirstResponder()
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        hideKeyboard()
-        return true
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let maxLength = 25
-        let currentString: NSString = (textField.text ?? "") as NSString
-        let newString: NSString =
-            currentString.replacingCharacters(in: range, with: string) as NSString
-        return newString.length <= maxLength
-    }
-    
-    @objc private func didTapStatusButton() {
-        self.statusLabel.text = self.statusTextField.text
-        if self.statusLabel.text == "" {
-            self.statusLabel.text = self.statusText
+    @objc private func didTapSetStatusButton() {
+        guard let status = statusTextField.text else {return}
+        if !status.isEmpty {
+            UIView.animate(withDuration: 0.5) {
+                self.statusLabel.text = self.statusTextField.text
+                self.statusTextField.text = .none
+            } completion: { _ in
+            }
         }
-        self.statusTextField.text = ""
+        if status.isEmpty {
+            statusTextField.shake()
+        }
+        endEditing(true)
     }
-    
 }
